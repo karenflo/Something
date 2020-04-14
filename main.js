68663c6c2c37f45693fb812a0ebd618c9652de59
Rosario Pulella <RosarioPulella@gmail.com>
2020-04-13 21:45:50 -0400

    Comment and put things togehte

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 // MODEL
+
+// The play size
 const PlayArea = {
     Width : 1000,
     Height : 1000
 }
 
+// The game window size
 const WindowSize = {
     Width : 800,
     Height : 800
 };
 
+// The Initial state
 const InitialState = 
 {
     player : {
@@ -24,6 +28,7 @@ const InitialState =
 
 // VIEW
 
+// Our background
 var background = document.createElement("rect");
 background.fill = "black";
 background.width = "100%";
@@ -31,32 +36,32 @@ background.height = "100%";
 
 const Background = background;
 
+// Transform a dimension from game space to window space
 function renderVal(play, window, value){
     return (value / play) * window;
 } 
 
+// Make a positioned circle from a play object.
 function renderPlayer(player){
     var view = document.createElement("circle");
     view.fill = "blue";
     view.cx = renderVal(PlayArea.Width, WindowSize.Width, player.position.x);
     view.cy = renderVal(PlayArea.Height, WindowSize.Height, player.position.y);
     
-    window.console.log(view);
-    
     return view;
 }
 
+// Transom the game state to an svg
 function render(state){
     var a = document.createElement("svg");
     a.width = WindowSize.Width;
     a.height = WindowSize.Height;
 
-    a.children = [
-        Background,
-        renderPlayer(state.player)
-    ];
+    a.append
+        ( Background
+        , renderPlayer(state.player)
+        );
 
-    window.console.log(a);
 
     return a;
 }
@@ -65,8 +70,6 @@ window.onload = function() {
     var game = document.getElementById("game");
     var svg = render(InitialState);
 
-    game.children = [
-        svg
-    ];
+    game.append(svg);
 
 };
